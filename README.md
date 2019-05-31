61485686a58fc92710f1c2aa9473b007d0a42286
CHEN Xian-an <xianan.chen@gmail.com>
2019-06-01 01:43:25 +0800

    RR 0.7 with the new ppx_bsx

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # ppx_bsx_example
 
-Run `yarn` or `npm install` and here you go.
+## Install `ppx_bsx`
+
+Clone this repo, and cd to its dir.
+
+```sh
+opam switch create . 4.06.1
+eval $(opam env)
+opam update
+opam install ppx_bsx
+```
+
+Config `bsconfig.json`:
+
+```json
+{
+  "ppx-flags": [
+    "./_opam/bin/ppx_bsx",
+    "./node_modules/bs-platform/lib/bsppx.exe -bs-jsx 3"
+  ]
+}
+```
+
+## Install npm dependencies
+
+Run `yarn` or `npm install`.
+
+## Run
 
 - for development: `yarn dev` or `npm run dev`
 - for production / distribution: `yarn dist` or `npm run dist`
--- src/welcome.ml
+++ src/welcome.ml
@@ -1,19 +1,14 @@
 external class_name : string = "welcome" [@@bs.module "./style"]
 
-let comp = ReasonReact.statelessComponent "Welcome"
+let make ~name  =
+  [%bsx "
+  <div className="class_name">
+    <img src="[%bs.raw {|require('./logo.png')|}]" />
+    <h1>
+      "("Hello " ^ name |> React.string)"
+    </h1>
+    <p>"(React.string {j|你好，世界！|j})"</p>
+  </div>
+  "]
+[@@react.component]
 
-let make ~name _ =
-  { comp with
-    render =
-      (fun _ ->
-         [%bsx "
-         <div className="class_name">
-           <img src="[%bs.raw {|require('./logo.png')|}]" />
-           <h1>
-             "("Hello " ^ name |> ReasonReact.string)"
-          </h1>
-          <p>"{|你好，世界！|}"</p>
-         </div>
-        "]
-      )
-  };
